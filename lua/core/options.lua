929fd49dba9555bb642c541a0e62087cabfb7bcf
Abhikalp Unakal <abhikalpunakal@outlook.com>
2024-06-09 11:22:39 -0700

    init nvim configs

---
+++ lua/core/options.lua
@@ -0,0 +1,82 @@
+local opt = vim.opt
+
+-- Session Management
+opt.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- Line Numbers
+opt.relativenumber = true
+opt.number = true
+
+-- Tabs & Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+vim.bo.softtabstop = 2
+
+-- Line Wrapping
+opt.wrap = false
+
+-- Search Settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- spellings
+opt.spell = false
+
+-- Cursor Line
+opt.cursorline = true
+
+-- Appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+vim.diagnostic.config {
+  float = { border = "rounded" }, -- add border to diagnostic popups
+}
+
+-- Backspace
+opt.backspace = "indent,eol,start"
+
+-- Clipboard
+opt.clipboard:append("unnamedplus")
+
+-- Split Windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- Consider - as part of keyword
+opt.iskeyword:append("-")
+
+-- Disable the mouse while in nvim
+opt.mouse = ""
+
+-- Folding
+opt.foldlevel = 20
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()" -- Utilize Treesitter folds
+
+-- Terminal
+vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
+    pattern = { "*" },
+    callback = function()
+        if vim.opt.buftype:get() == "terminal" then
+            vim.cmd(":startinsert")
+            vim.cmd(":tnoremap <buffer> <Esc> <c-\\><c-n>")
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd({"VimEnter"},{
+  pattern = {"*"},
+  callback = function()
+    vim.cmd(":NvimTreeFocus");
+  end
+})
+
+vim.api.nvim_create_autocmd({"VimLeave"},{
+  pattern = {"*"},
+  callback = function()
+      vim.cmd(":mksession ~/.last")
+  end
+})
